34694899ff17a0e7d86c2303ece848c126f4404f
Anish Bade <badeanish6045@gmail.com>
2024-06-29 17:07:25 +0545

    create necessary views and urls

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from rest_framework.response import Response
+
+from .serializers import UserSerializer
+
+
+def register_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+
+    return Response(serializer.errors , status = 400)
+
