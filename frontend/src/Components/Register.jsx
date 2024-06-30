6da32b67fd869fc365dd447207647386dc833a54
Anish Bade <badeanish6045@gmail.com>
2024-06-30 22:14:06 +0545

    user registration frontend

--- frontend/src/Components/Register.jsx
+++ frontend/src/Components/Register.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
 import { TextField, Button } from '@mui/material';
+import { useState } from 'react';
 
 export default function Register() {
+  const BASE_URL = 'http://localhost:8000/';
+  const [ formData, setFormData ] = useState({
+    'email': '',
+    'first_name': '',
+    'last_name': '',
+    'password': ''
+  })
+  const handleFormSubmit = () => {
+    fetch(`${BASE_URL}register/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
   return (
     <>
       <div className='container text-center'>
         <div className='mt-3'>
-          <TextField id="email" type='email' label="Email" variant="outlined" />
+          <TextField id="email" type='email' label="Email" variant="outlined"  onChange={e => setFormData({...formData, email: e.target.value})}/>
         </div>
         <div className='mt-3'>
-          <TextField id="first_name" type='text' label="First Name" variant="outlined" />
+          <TextField id="first_name" type='text' label="First Name" variant="outlined" onChange={e => setFormData({...formData, first_name: e.target.value})}/>
         </div>
         <div className='mt-3'>
-          <TextField id="last_name" type='text' label="Last Name" variant="outlined" />
+          <TextField id="last_name" type='text' label="Last Name" variant="outlined" onChange={e => setFormData({...formData, last_name: e.target.value})}/>
         </div>
         <div className='mt-3'>
-          <TextField id="outlined-basic" type='password' label="Password" variant="outlined" />
+          <TextField id="password" type='password' label="Password" variant="outlined" onChange={e => setFormData({...formData, password: e.target.value})}/>
         </div>
 
         <div className='mt-3'>
-          <Button variant='contained'>Save</Button>
+          <Button variant='contained' onClick={handleFormSubmit}>Save</Button>
         </div>
 
 
