abc2d14695e7acf1b0c0d8abf62f5c4cc40e3330
Anish Bade <badeanish6045@gmail.com>
2024-06-30 17:27:51 +0545

    registration flow done

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,15 +1,31 @@
 from rest_framework.decorators import api_view
-
+from rest_framework import status
 from rest_framework.response import Response
+from accounts.tokenauthentication import JWTAuthentication
+from .serializers import LoginSerializer, UserSerializer
 
-from .serializers import UserSerializer
 
-@api_view(['POST'])
+@api_view(["POST"])
 def register_user(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=201)
 
-    return Response(serializer.errors , status = 400)
+    return Response(serializer.errors, status=400)
 
+
+@api_view(["POST"])
+def login(request):
+    serializer = LoginSerializer(request.data)
+    if serializer.is_valid():
+        token = JWTAuthentication.generate_token(payload=serializer.data)
+        return Response(
+            {
+                "message": "Login Successfull.",
+                "token": token,
+                "user": serializer.data,
+            },
+            status=status.HTTP_201_CREATED
+        )
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,3 +126,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 AUTH_USER_MODEL = 'accounts.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'accounts.tokenauthentication.JWTAuthentication'
+    ]
+}
