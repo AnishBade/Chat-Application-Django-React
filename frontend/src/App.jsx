9cfe14eb2719917ea6fe35d063a34c0891985a7b
Anish Bade <badeanish6045@gmail.com>
2024-07-02 21:10:35 +0545

    frontend login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Register from './Components/Register'
-
+import Login from './Components/Login'
 export default function App() {
   return (
-    <Register/>
+    <>
+      <Register />
+      <Login />
+    </>
   )
 }
