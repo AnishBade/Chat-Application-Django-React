85e476c405c858835f133269ea2fda5da3c37cf9
Anish Bade <badeanish6045@gmail.com>
2024-07-03 08:34:33 +0545

    added routing for login and register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Register from './Components/Register'
 import Login from './Components/Login'
+import Navigate from './Components/Navigate'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 export default function App() {
   return (
-    <>
-      <Register />
-      <Login />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path='/' element={<Navigate />}/>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+      </Routes>
+
+    </Router>
+
+  );
 }
